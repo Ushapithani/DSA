3d29e0b402f2a1186aa2f2557327190dad162ec5
pithaniusha <pithaniusha73@gmail.com>
2026-01-22 21:08:11 +0530

    changes done

--- DAY 5/url.py
+++
@@ -1,9 +0,0 @@
-url = input("Enter a URL: ")
-
-if url.startswith("http://") or url.startswith("https://"):
-    if "." in url:
-        print(" URL is valid")
-    else:
-        print(" URL is invalid ")
-else:
-    print(" URL is invalid")
